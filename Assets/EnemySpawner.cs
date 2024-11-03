dd740bd510e37cbc45c5378e09abe7f83e45bd25
Kevin Ruvalcaba <127262121+Kruvalcaba85@users.noreply.github.com>
2024-11-02 18:38:23 -0700

    Redid zombie spawning system, system is now wave and time based.

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -4,28 +4,96 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [System.Serializable]
+    public class Wave
+    {
+        public string waveName;
+        public List<EnemyGroup> enemyGroups;
+        public int waveQuota; // total number of enemies to spawn in wave
+        public float spawnInterval; // the interval at which to spawn enemies
+        public float spawnCount; // the number of enemies spawned in current wave
+
+    }
+
+    [System.Serializable]
+    public class EnemyGroup
+    {
+        public string enemyName;
+        public int enemyCount; //number of enemies in the wave
+        public int spawnCount; //number of enemies of the current type already spawned in
+        public GameObject enemyPrefab; 
+    }
+
+    public List<Wave> waves; // A list of all the waves in the game
+    public int currentWaveCount; // index of current wave
+
+    [Header("Spawner Atrributes")]
+    float spawnTimer; // timer for determining when to spawn next enemy
+    public int enemiesAlive;
+    public int maxEnemiesAllowed; //max amount of enemies allowed at once
+    public bool maxEnemiesReached = false;
+    public float waveInterval; // interval between waves
+
+
+    Transform player;
+    [Header("Spawn Positions")]
+    public List<Transform> relativeSpawnPoints; // list storing all relative spawn points
+
+
     [SerializeField]
     private GameObject zombiePrefab;
 
     [SerializeField]
-    private float zombieInterval = 0.001f;
+    //private float zombieInterval = 0.001f;
 
 
-    private bool spawning = true;
+    //private bool spawning = true;
     // Start is called before the first frame update
     void Start()
     {
+        player = FindObjectOfType<playerController>().transform;
+        CalculateWaveQuota();
         //StartCoroutine(spawnEnemy(zombieInterval, zombiePrefab));
-        StartCoroutine(SpawnEnemies());
+        //StartCoroutine(SpawnEnemies());
     }
 
+    private void Update()
+    {
+        if(currentWaveCount < waves.Count && waves[currentWaveCount].spawnCount == 0) //check if wave has ended and next wave should start
+        {
+            StartCoroutine(BeginNextWave());
+        }
+        spawnTimer += Time.deltaTime;
+
+        //check if its time to spawn next enemy
+        if(spawnTimer >= waves[currentWaveCount].spawnInterval)
+        {
+            spawnTimer = 0f;
+            SpawnEnemies();
+        }
+    }
+
+    IEnumerator BeginNextWave()
+    {
+        //wave for 'waveinterval' seconds before next wave
+        yield return new WaitForSeconds(waveInterval);
+
+        //if there are still waves to go, move on to next wave
+        if(currentWaveCount < waves.Count - 1)
+        {
+            currentWaveCount++;
+            CalculateWaveQuota();
+        }
+    }
+    /*
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
         GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6), 0), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
     }
-
+    */
+    /*
     private IEnumerator SpawnEnemies()
     {
         while (spawning)
@@ -33,5 +101,52 @@ public class EnemySpawner : MonoBehaviour
             GameObject newEnemy = Instantiate(zombiePrefab, new Vector3(Random.Range(-5f, -5f), Random.Range(-6f, 6f), 0), Quaternion.identity);
             yield return new WaitForSeconds(zombieInterval);
         }
+    }
+    */
+    void CalculateWaveQuota()
+    {
+        int currentWaveQuota = 0;
+        foreach(var enemyGroup in waves[currentWaveCount].enemyGroups)
+        {
+            currentWaveQuota += enemyGroup.enemyCount;
+        }
+
+        waves[currentWaveCount].waveQuota = currentWaveQuota;
+        Debug.LogWarning(currentWaveQuota);
+    }
+    
+    void SpawnEnemies()
+    {
+        //checks if the minimum number of enemies in the wave have been spawned
+        if (waves[currentWaveCount].spawnCount < waves[currentWaveCount].waveQuota && !maxEnemiesReached)
+        {
+            //spawn each type of enemy until quota is filled
+            foreach(var enemyGroup in waves[currentWaveCount].enemyGroups)
+            {
+                // check if the minimum number of enemies of this type have been spawned
+                if(enemyGroup.spawnCount < enemyGroup.enemyCount)
+                {
+                    if(enemiesAlive >= maxEnemiesAllowed)
+                    {
+                        maxEnemiesReached = true;
+                        return;
+                    }
+                    Instantiate(enemyGroup.enemyPrefab, player.position + relativeSpawnPoints[Random.Range(0, relativeSpawnPoints.Count)].position, Quaternion.identity); 
+                    enemyGroup.spawnCount++;
+                    waves[currentWaveCount].spawnCount++;
+                    enemiesAlive++;
+                }
+            }
+        }
+        //reset flag if the number of enemies is less than the allowed amount
+        if(enemiesAlive < maxEnemiesAllowed)
+        {
+            maxEnemiesReached = false;
+        }
+    }
+
+    public void OnEnemyKilled()
+    {
+        enemiesAlive--;
     }
 }
--- Assets/ZombieStats.cs
+++ Assets/ZombieStats.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
@@ -11,9 +12,13 @@ public class ZombieStats : MonoBehaviour
     public float dropChance = 0.5f;
     public int damagetoPlayer = 2;
     private Weapon weapon;
+
+    public float despawnDistance = 20f;
+    Transform player;
     // Start is called before the first frame update
     void Start()
     {
+        player = FindObjectOfType<playerController>().transform;
         currentHealth = health;
         weapon = FindObjectOfType<Weapon>();
     }
@@ -24,6 +29,11 @@ public class ZombieStats : MonoBehaviour
         if (currentHealth <= 0)
         {
             Die();
+        }
+
+        if(Vector2.Distance(transform.position, player.position) >= despawnDistance)
+        {
+            ReturnEnemy();
         }
     }
 
@@ -44,5 +54,22 @@ public class ZombieStats : MonoBehaviour
         Destroy(gameObject);
         weapon.addWeaponExperience();
         
+    }
+
+    public void OnDestroy()
+    {
+        if (Application.isPlaying)
+        {
+            EnemySpawner es = FindObjectOfType<EnemySpawner>();
+            if (es != null)
+            {
+                es.OnEnemyKilled();
+            }
+        }
+    }
+    void ReturnEnemy()
+    {
+        EnemySpawner es = FindObjectOfType<EnemySpawner>();
+        transform.position = player.position + es.relativeSpawnPoints[Random.Range(0, es.relativeSpawnPoints.Count)].position;
     }
 }
